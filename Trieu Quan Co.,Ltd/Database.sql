58cb6447472ab4e9a276ba2f9b7a42b8317f80d9
Suimei1208 <103234140+Suimei1208@users.noreply.github.com>
2023-04-22 22:51:10 +0700

    Update Database.sql

--- Trieu Quan Co.,Ltd/Database.sql
+++ Trieu Quan Co.,Ltd/Database.sql
@@ -1,1 +1,14 @@
 CREATE DATABASE Final_SE
+
+CREATE TABLE Accountants (
+    accountant_id INT PRIMARY KEY,
+    accountant_name VARCHAR(50),
+    username VARCHAR(50),
+    password VARCHAR(50),
+);
+INSERT INTO Accountants
+VALUES (1, 'Admin', 'admin', '123456789'),
+       (2, 'Trieu', 'suimei', '123456789'),
+       (3, 'Quan', 'quan', 'p@123456789');
+
+drop table Accountants
