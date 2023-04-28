683ca58c1dd03b8c1358010c051312aeba61a1c7
Suimei1208 <103234140+Suimei1208@users.noreply.github.com>
2023-04-28 20:02:47 +0700

    .

--- Trieu Quan Co.,Ltd/Database.sql
+++ Trieu Quan Co.,Ltd/Database.sql
@@ -26,22 +26,37 @@ create table tblStockReceiptDetails(
 	FOREIGN KEY (StockReceiptCode) REFERENCES tblStockReceipt(ID)
 	);
 
-create table tblStockIssue(
-	ID varchar(50) PRIMARY KEY,
-	DateOfIssue date,
-	DistributorCode varchar(50),
-	TotalValue decimal,
-	Status varchar(50)
+CREATE TABLE Distributor (
+	ID VARCHAR(50) PRIMARY KEY,
+	Username VARCHAR(50),
+	Password VARCHAR(50),
+	Name VARCHAR(50),
+	Phone VARCHAR(50),
+	Email VARCHAR(50)
 );
 
-create table tblStockIssueDetails(
-	ID varchar(50) PRIMARY KEY,
-	ExportCode varchar(50),
-	ProductCode varchar(50),
-	Quantity decimal,
-	UnitPrice decimal,
-	TotalAmount decimal,
-	FOREIGN KEY (ExportCode) REFERENCES tblStockIssue(ID)
+CREATE TABLE StockIssue (
+	ID VARCHAR(50) PRIMARY KEY,
+	DateOfIssue DATE,
+	DistributorCode VARCHAR(50),
+	TotalValue DECIMAL,
+	Status VARCHAR(50),
+	PaymentMethod VARCHAR(50),
+	PaymentStatus VARCHAR(50),
+	accountant_id INT,
+	FOREIGN KEY (accountant_id) REFERENCES Accountants(accountant_id),
+	FOREIGN KEY (DistributorCode) REFERENCES Distributor(ID)
+);
+
+
+CREATE TABLE StockIssueDetails (
+	ID VARCHAR(50) PRIMARY KEY,
+	ExportCode VARCHAR(50),
+	ProductCode VARCHAR(50),
+	Quantity DECIMAL,
+	UnitPrice DECIMAL,
+	TotalAmount DECIMAL,
+	FOREIGN KEY (ExportCode) REFERENCES StockIssue(ID)
 );
 
 create table Products(
@@ -64,36 +79,33 @@ AFTER INSERT, UPDATE
 AS
 BEGIN
 -- Kiểm tra nếu trigger được kích hoạt bởi INSERT
-IF EXISTS(SELECT * FROM inserted)
-BEGIN
-UPDATE Products
-SET Quantity = Products.Quantity + (SELECT SUM(Quantity) FROM inserted WHERE ProductCode = Products.ProductID),
-UnitPrice = (SELECT UnitPrice FROM inserted WHERE ProductCode = Products.ProductID)
-WHERE ProductID IN (SELECT ProductCode FROM inserted);
-END
+	IF EXISTS(SELECT * FROM inserted)
+	BEGIN
+		UPDATE Products
+		SET Quantity = Products.Quantity + (SELECT SUM(Quantity) FROM inserted WHERE ProductCode = Products.ProductID),
+		UnitPrice = (SELECT UnitPrice FROM inserted WHERE ProductCode = Products.ProductID)
+		WHERE ProductID IN (SELECT ProductCode FROM inserted);
+	END
 -- Kiểm tra nếu trigger được kích hoạt bởi UPDATE
-ELSE IF EXISTS(SELECT * FROM deleted)
-BEGIN
-UPDATE p
-SET Quantity = CASE
-WHEN p.Quantity <= i.Quantity
-THEN p.Quantity + (i.Quantity - d.Quantity)
-ELSE
-p.Quantity - (d.Quantity - i.Quantity)
-END,
-UnitPrice = i.UnitPrice
-FROM Products p
-INNER JOIN deleted d ON p.ProductID = d.ProductCode
-INNER JOIN inserted i ON i.ID = d.ID
-WHERE p.ProductID IN (SELECT ProductCode FROM inserted UNION SELECT ProductCode FROM deleted);
-END
+	ELSE IF EXISTS(SELECT * FROM deleted)
+	BEGIN
+		UPDATE p
+		SET Quantity = CASE
+		WHEN p.Quantity <= i.Quantity
+		THEN p.Quantity + (i.Quantity - d.Quantity)
+		ELSE
+		p.Quantity - (d.Quantity - i.Quantity)
+		END,
+		UnitPrice = i.UnitPrice
+		FROM Products p
+		INNER JOIN deleted d ON p.ProductID = d.ProductCode
+		INNER JOIN inserted i ON i.ID = d.ID
+		WHERE p.ProductID IN (SELECT ProductCode FROM inserted UNION SELECT ProductCode FROM deleted);
+	END
 
 END;
 
 GO
-
-
-
 
 CREATE TRIGGER trg_UpdateProductQuantity
 ON tblStockReceiptDetails
@@ -109,10 +121,12 @@ END
 
 select * from Products
 select * from tblStockReceipt
-delete from tblStockReceipt
-delete from tblStockReceiptDetails
+delete from StockIssue
+delete from StockIssueDetails
 
 SELECT TOP 1 ProductID FROM Products ORDER BY ProductID DESC
+
+
 
 INSERT INTO Accountants
 VALUES (1, 'Admin', 'admin', '123456789'),
@@ -137,3 +151,34 @@ Insert into Suppliers(SupplierID, SupplierName, PhoneNumber, Email) values
 ('S0003', 'MNO Corporation', '0909090909', 'mno@outlook.com'),
 ('S0004', 'PQR Limited', '0777777777', 'pqr@hotmail.com'),
 ('S0005', 'LMN Limited', '0888888888', 'lmn@gmail.com')
+
+INSERT INTO Distributor (ID, Username, Password, Name, Phone, Email)
+VALUES
+('DL001', 'user1', '123456', 'John Doe', '123456789', 'john.doe@example.com'),
+('DL002', 'user2', '123456', 'Jane Doe', '987654321', 'jane.doe@example.com'),
+('DL003', 'user3', '123456', 'Bob Smith', '012345678', 'bob.smith@example.com'),
+('DL004', 'user4', '123456', 'Alice Johnson', '555555555', 'alice.johnson@example.com'),
+('DL005', 'user5', '123456', 'David Lee', '111111111', 'david.lee@example.com'),
+('DL006', 'user6', '123456', 'Emily Davis', '222222222', 'emily.davis@example.com'),
+('DL007', 'user7', '123456', 'Steven Brown', '333333333', 'steven.brown@example.com'),
+('DL008', 'user8', '123456', 'Lisa Chen', '444444444', 'lisa.chen@example.com'),
+('DL009', 'user9', '123456', 'Michael Wilson', '666666666', 'michael.wilson@example.com'),
+('DL010', 'user10', '123456', 'Samantha Lee', '777777777', 'samantha.lee@example.com');
+
+
+--Test
+INSERT INTO StockIssue (ID, DateOfIssue, DistributorCode, TotalValue, Status, PaymentMethod, PaymentStatus)
+VALUES
+('SI001', '2022-01-01', 'DL001', 100000, 'pending', 'cash', 'unpaid'),
+('SI002', '2022-01-02', 'DL002', 200000, 'delivered', 'credit card', 'paid'),
+('SI003', '2022-01-03', 'DL003', 300000, 'cancelled', 'bank transfer', 'unpaid'),
+('SI004', '2022-01-04', 'DL004', 400000, 'pending', 'cash', 'unpaid'),
+('SI005', '2022-01-05', 'DL005', 500000, 'delivered', 'credit card', 'paid');
+
+INSERT INTO StockIssueDetails (ID, ExportCode, ProductCode, Quantity, UnitPrice, TotalAmount)
+VALUES
+('SID001', 'SI001', 'PD001', 10, 5000, 50000),
+('SID002', 'SI001', 'PD002', 20, 10000, 200000),
+('SID003', 'SI002', 'PD003', 30, 15000, 450000),
+('SID004', 'SI003', 'PD004', 40, 20000, 800000),
+('SID005', 'SI004', 'PD005', 50, 25000, 1250000);
